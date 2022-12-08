5b341c4fbaba6c1c565d42e1a22c242f15396d35
Kamgo N Elton <kamgoelton89@gmail.com>
2022-12-08 01:01:01 +0100

    fixes

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,4 +6,4 @@ def complex_delete(a_dictionary, value):
         if value == a_dictionary.get(value_chosen):
             del a_dictionary[value_chosen]
 
-        return (a_dictionary)
+    return (a_dictionary)
