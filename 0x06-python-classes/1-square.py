36f80989c37d9d61747204f45a91b7c392b9d83d
Kamgo N Elton <kamgoelton89@gmail.com>
2022-12-20 14:07:18 -0800

    made executable

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,10 +2,11 @@
 
 """Define a Square."""
 
+
 class Square:
     """Represent a square"""
 
-    def __init__(self,size):
+    def __init__(self, size):
         """Initiatialize a new Square.
         Args:
             size (int): The size of the new square.
