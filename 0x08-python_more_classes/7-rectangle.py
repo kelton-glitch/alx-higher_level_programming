2bc2574eb89168277a970de6a094b1f6d27e9ade
Kamgo N Elton <kamgoelton89@gmail.com>
2023-01-02 12:44:27 -0800

    made executable

--- 0x08-python_more_classes/7-rectangle.py
+++ 0x08-python_more_classes/7-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self,value):
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -41,7 +41,7 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self,value):
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
