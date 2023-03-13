88660d9002d231a6e530d592101b60ca02557124
Kamgo N Elton <kamgoelton89@gmail.com>
2023-03-14 00:06:31 +0100

    fixes

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,11 @@
-const myObject={
-    type: 'object',
-    value: 12
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
-myObject.incr = function() {
-    this.value++;
+myObject.incr = function () {
+  this.value++;
 };
 myObject.incr();
 console.log(myObject);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if(isNaN(process.argv[2]) || process.argv[2] === undefined) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
     console.log('Not a number');
-} else {
-    console.log('My number: ', parseInt(process.argv[2]));
-}
+  } else {
+    console.log('My number:', parseInt(process.argv[2]));
+  }
+  
