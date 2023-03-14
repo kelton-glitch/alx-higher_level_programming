5c3fa4b5656a865da818d3c89d06c9ecd2ea6d3e
root <kamgoelton89@gmail.com>
2023-03-14 10:38:30 +0000

    fixes

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,13 @@
 #!/node/usr/node
-class Rectangle{
-    constructor(w,h) {
-        this.width = w;
-        this.height = h;
-        if (w || h === 0){
-            this.height = "";
-            this.width = "";
-        }
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+    if (w || h === 0) {
+      this.height = '';
+      this.width = '';
     }
+  }
 }
 
 module.exports = Rectangle;
