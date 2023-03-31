8456fe470fd91bd87c0cd58ed23d8adba6a195b5
Kamgo N Elton <kamgoelton89@gmail.com>
2023-03-31 16:42:32 +0100

    fixes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that
+"""A script that
 - fetches https://alx-intranet.hbtn.io/status.
 - uses urlib package
 """
@@ -7,10 +7,10 @@
 
 if __name__ == '__main__':
     import urllib.request
-    
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
-        print("Body response: ")
-        print("\t- typeL {}".format(type(content)))
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
