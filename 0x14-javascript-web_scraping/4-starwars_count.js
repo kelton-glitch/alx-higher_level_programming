434436db434968d7815aab792accaad3a55da392
Kamgo N Elton <kamgoelton89@gmail.com>
2023-04-25 23:23:39 +0100

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     console.log(JSON.parse(body).results.reduce((count, film) => {
-      return film.characters.find((character) => character.endsWith('/18'))
+      return film.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
