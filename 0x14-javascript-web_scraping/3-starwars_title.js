a83b29b5f63c0f0005c3f12a2cebc7a2a97d2d1c
Kamgo N. Elton <kamgoelton89@gmail.com>
2023-07-26 23:57:19 +0000

    web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,15 +2,15 @@
 
 const request = require('request');
 const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.alx-tools.com/api/films/:id';
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episodeNum, function (error, response, body) {
-	if (error) {
-		console.log(error);
-	} else if (response.statusCode === 200) {
-		const responseJSON = JSON.parse(body);
-		console.log(responseJSON.title);
-	} else {
-		console.log('Error code: ' + response.statusCode);
-	}
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
 });
